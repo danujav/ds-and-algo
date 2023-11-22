c06b53e652ff437c0018e5ee41f88dbe2ad25caf
danu <danujagreru@gmail.com>
2023-11-22 10:26:42 +0530

    Stack push and peek

---
+++ src/lk/ijse/ds/stack/Main.java
@@ -0,0 +1,25 @@
+package lk.ijse.ds.stack;
+
+/*
+    @author DanujaV
+    @created 11/22/23 - 10:13 AM   
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        int peek = stack.peek();
+        System.out.println("Peek: " + peek);
+
+        stack.push(40);
+        System.out.println("Peek: " + stack.peek());
+
+        stack.push(50);
+        System.out.println("Peek: " + stack.peek());
+    }
+}
---
+++ src/lk/ijse/ds/stack/Stack.java
@@ -0,0 +1,24 @@
+package lk.ijse.ds.stack;
+
+/*
+    @author DanujaV
+    @created 11/22/23 - 10:11 AM   
+*/
+
+public class Stack {
+    private int[] elementData;
+    private int top;    //points to the top of the stack
+    
+    public Stack(int initialCapacity){
+        elementData = new int[initialCapacity];
+        top = -1;
+    }
+    
+    public void push(int data) {
+        elementData[++top] = data;
+    }
+
+    public int peek() { //top
+        return elementData[top];
+    }
+}
