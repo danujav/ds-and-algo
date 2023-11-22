a3777d9830706dc3501f07db8943e265572c78ae
danu <danujagreru@gmail.com>
2023-11-22 14:07:29 +0530

    Node class created

---
+++ src/lk/ijse/ds/linkedlist/LinkedList.java
@@ -0,0 +1,24 @@
+package lk.ijse.ds.linkedlist;
+
+/*
+    @author DanujaV
+    @created 11/22/23 - 2:01 PM   
+*/
+/*class Node {
+    private int data;
+    private Node next;
+}*/
+
+public class LinkedList {
+    private static class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+    private Node head;
+
+
+}
---
+++ src/lk/ijse/ds/linkedlist/Main.java
@@ -0,0 +1,9 @@
+package lk.ijse.ds.linkedlist;
+
+/*
+    @author DanujaV
+    @created 11/22/23 - 2:01 PM   
+*/
+
+public class Main {
+}
